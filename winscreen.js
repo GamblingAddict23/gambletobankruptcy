49ae216d20e518e997a6a6d7363da176ec5416f1
a <a>
2024-12-03 15:14:35 -0800

    ye

---
+++ winscreen.js
@@ -0,0 +1,9 @@
+//this is all Max's code
+
+function declined() {
+window.location.href = "https://mn584.github.io/gambletobankruptcy/slots.html"
+}
+
+function accepted() {
+    window.location.href = "https://raidshadowlegends.com"
+}
