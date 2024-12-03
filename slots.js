db594cfd95a04f44b57a0d19f97d2d4479b79b12
a <a>
2024-12-03 15:14:11 -0800

    big steppa

--- slots.js
+++ slots.js
@@ -1,19 +1,25 @@
+//declares variables to get the slots from slots.html
 let spinnerOneEl = document.getElementById("slotValueOne")
 let spinnerTwoEl = document.getElementById("slotValueTwo")
 let spinnerThreeEl = document.getElementById("slotValueThree")
+//declares variables to get values for slot numbers
 let valueOne = 0
 let valueTwo = 0
 let valueThree = 0
 
-function spin() {
-  slotButton.hidden = true
 
+function spin() {
+  slotButton.hidden = true //hides the spin button so it can't be hit repeatedly
+
+  //randomizes all three values
   valueOne = Math.floor(Math.random() * 10)
   valueTwo = Math.floor(Math.random() * 10)
   valueThree = Math.floor(Math.random() * 10)
+  //changes the numbers on screen to the randomized values
   slotValueOne.innerText = valueOne
   slotValueTwo.innerText = valueTwo
   slotValueThree.innerText = valueThree
+  //gives class "seven" if a seven is hit and removes the class if it is any other number
   if (valueOne == 7) {
     spinnerOneEl.classList.add("seven")
   } else {
@@ -29,7 +35,7 @@ function spin() {
   } else {
     spinnerThreeEl.classList.remove("seven")
   }
-
+  //plays the spinning audio
   document.getElementById("yay").play()
 
 }
@@ -38,27 +44,29 @@ function seven() {
   valueOne = 7
   valueTwo = 7
   valueThree = 7
+  slotValueOne.innerText = valueOne
+  slotValueTwo.innerText = valueTwo
+  slotValueThree.innerText = valueThree
   checkSpin()
 }
 
+//this is run when the spin button is clicked
 function clickSpin() {
 
-  let repeatInterval = setInterval(spin, 50) // happens every 50ms (0.05s)
+  let repeatInterval = setInterval(spin, 50) //runs the spin function every 50ms (0.05s)
   setTimeout(function () {
-    clearInterval(repeatInterval) // stops repeatThis
-    slotButton.hidden = false
-    checkSpin() // runs at the end
-  }, 3700) // will happen after 3700ms (3.7s)
+    clearInterval(repeatInterval) //stops repeatThis
+    slotButton.hidden = false //makes spin button reappear
+    checkSpin() //runs checkSpin function
+  }, 3700) //runs these 3 lines after 3700ms (3.7s)
 
 }
 
+//runs when 777 is hit
 function checkSpin() {
   if (valueOne == 7 && valueTwo == 7 && valueThree == 7) {
-    slotButton.hidden = true
-    window.location.href = "https://www.mn584.github.io/gambletobankruptcy/winscreen.html"
+    slotButton.hidden = true //hides button
+    window.location.href = "https://mn584.github.io/gambletobankruptcy/winscreen.html" //opens winscreen.html
   }
 }
-
-
-//max's code from here
 
